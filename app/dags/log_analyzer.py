dba93abdfcd03cc2ab3510e21b3590338a094f53
Creohex <paradise.source@gmail.com>
2019-04-16 03:34:26 +0300

    complete dags (testing required)

--- app/dags/log_analyzer.py
+++ app/dags/log_analyzer.py
@@ -3,15 +3,14 @@ from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python_operator import PythonOperator
 from datetime import datetime, timedelta
 from dateutil import parser
-import psycopg2
-import common
+import psycopg2, os, common
 
 
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
     'start_date': common.datetime_hour_truncated(datetime.now()).replace(hour=0),
-    'email': ['airflow@example.com'],
+    'email': ['example@mailbox.flock'],
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
@@ -22,19 +21,27 @@ default_args = {
 dag = DAG('log_analyzer', default_args=default_args, 
           schedule_interval=timedelta(hours=1))
 
+# TODO: test...
 def analyze_hourly(ds, **kwargs):
     try:
-        ts = parser.parse(kwargs['ts'])
-        # TODO: ...
-        return "hourly results..."
+        ts = common.datetime_hour_truncated(parser.parse(kwargs['ts'])) # scheduled hour
+        logs = common.select_logs(ts)
+        for level in common.select_log_levels():
+            num_messages = len(list(filter(lambda x: x[1] == level), logs))
+            common.insert_hourly(ts, level, num_messages)
+        return "Done calculating hourly logs (%s)." % ts
     except Exception as e:
         return "Error in 'analyze_hourly'. Info: \n\n%s" % str(e)
 
+# TODO: test...
 def analyze_indicents(ds, **kwargs):
     try:
-        ts = parser.parse(kwargs['ts'])
-        # TODO: ...
-        return "incident results..."
+        ts = common.datetime_hour_truncated(parser.parse(kwargs['ts'])) # scheduled hour
+        E = int(os.environ['E'])
+        num = next((_ for _ in common.select_hourly(ts) if _[1] == "ERROR"), None)[2]
+        if num > E:
+            common.insert_incident(ts, num)
+        return "Done analyzing indicents."
     except Exception as e:
         return "Error in 'analyze_incidents'. Info: \n\n%s" % str(e)
 
