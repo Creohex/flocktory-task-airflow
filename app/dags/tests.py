89be5a2d22c5e557e6d4aa61e502358b15ebbe15
Creohex <paradise.source@gmail.com>
2019-04-16 03:35:44 +0300

    Update tests.py (helpers, db connectivity)

--- app/dags/tests.py
+++ app/dags/tests.py
@@ -1,23 +1,47 @@
-from common import *
+import importlib
+import common
 from log_analyzer import analyze_hourly, analyze_indicents
-import os, psycopg2, unittest
+import os, psycopg2, unittest, time
+from datetime import datetime
+from dateutil import parser
 
 
-# TODO: ...
-# common:
-class TestCommonMethods(unittest.TestCase):
+class TestHelpers(unittest.TestCase):
     def test_datetime_hour_truncated(self):
-        self.assertTrue(True)
-        pass
+        self.assertEqual(
+            common.datetime_hour_truncated(
+                parser.parse('2019-12-25 02:30:45')),
+            parser.parse('2019-12-25 02:00:00'))
 
     def test_is_datetime_hour_truncated(self):
-        self.assertTrue(False)
-        pass
+        self.assertFalse(common.is_datetime_hour_truncated(
+            parser.parse('2019-12-25 02:30:45')))
+        self.assertTrue(common.is_datetime_hour_truncated(
+            parser.parse('2019-12-25 00:00:00')))
+    
+    def test_get_env_vars(self):
+        # expect not having all required variables
+        with self.assertRaises(Exception) as context:
+            common.get_env_vars()
+            self.assertTrue('missing required variables' in str(context.exception))
+        
+        # manually add required variables
+        for _ in ['PGHOST', 'PGUSER', 'PGPORT', 'PGPASSWORD', 'PGDATABASE', 'E']:
+            os.environ[_] = 'qwerty'
+
+        # expect function to succeed
+        common.get_env_vars()
 
 
-# TODO: ...
-# log_analyzer:
-class TestCommonMethods(unittest.TestCase):
+class TestDb(unittest.TestCase):
+    def test_get_connection(self):
+        print(common.get_env_vars())
+        common.get_connection()
+
+    def test_query_db(self):
+        self.assertEqual((True, [(1,)]), common.query_db("select 1"))
+
+class TestLogAnalyzer(unittest.TestCase):
     def test_analyze_hourly(self):
         pass
     
