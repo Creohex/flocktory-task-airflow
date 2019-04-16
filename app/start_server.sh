6abab2709ea192b2936fe7e5217906adaf1d0092
Creohex <paradise.source@gmail.com>
2019-04-17 00:04:46 +0300

    add optional test log generation on startup

--- app/start_server.sh
+++ app/start_server.sh
@@ -9,10 +9,16 @@ python ./tests.py
 
 if [ $? == "1" ]; then
     echo "tests failed, skipping airflow server/scheduler..."
-else
-    # init/start server + scheduler
-    airflow initdb
-    airflow list_dags
-    airflow webserver &
-    airflow scheduler
+    exit 1
 fi
+
+# insert logs for testing purposes
+if [ "$GENERATE_TEST_LOGS" == "true" ]; then
+    python ./db_insert_test_values.py
+fi
+
+# init/start server + scheduler
+airflow initdb
+airflow list_dags
+airflow webserver &
+airflow scheduler
