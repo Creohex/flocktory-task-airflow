c8448e03d7e9f8b7fdeb991ff7d3fc1b3785cb7c
Creohex <paradise.source@gmail.com>
2019-04-16 03:33:58 +0300

    Add airflow server/scheduler back into docker compose and update start logic

---
+++ app/dags/wait_for_db.py
@@ -0,0 +1,11 @@
+import common, time
+
+
+while True:
+    try:
+        common.get_connection()
+        print("successfully connected to DB")
+        break
+    except:
+        print("couldn't connect to DB, waiting...")
+        time.sleep(1)
--- app/start_server.sh
+++ app/start_server.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 echo "starting server..."
 
-airflow initdb
-airflow list_dags
-airflow webserver &
-airflow scheduler
+# wait for db to be accessible: ...
+python ./wait_for_db.py
+
+# run tests
+python ./tests.py
+
+if [ $? == "1" ]; then
+    echo "tests failed, skipping airflow server/scheduler..."
+else
+    # init/start server + scheduler
+    airflow initdb
+    airflow list_dags
+    airflow webserver &
+    airflow scheduler
+fi
